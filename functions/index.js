3401f2a0738db51881a4bf02992402a1bd485047
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2017-05-03 16:45:41 -0400

    add 'get download img url' feature

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,10 @@
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-
 const request = require('request');
 const crypto = require('crypto');
 const storage = require('@google-cloud/storage');
+
+admin.initializeApp(functions.config().firebase);
 
 // Firebase Project ID and Service Account Key.
 const gcs = storage({
@@ -16,57 +15,65 @@ const gcs = storage({
 const bucket = gcs.bucket('snapshelf-aabb55.appspot.com');
 
 exports.getProcessedImage = functions.https.onRequest((req, res) => {
-	console.log(req.body.processedImageURL);
-	console.log(req.body.imageTicket);
-	if (req.body && req.body.processedImageURL) {
-    	// Get image from Pixelz and save it to Firebase Storage.
-    	saveImage(
-    		req.body.processedImageURL, 
-    		req.body.imageTicket
-    	);
-    	return res.status(200).end();
-  	}
-  	res.status(400).end();
+  if (req.body && req.body.processedImageURL) {
+
+		// Get image from Pixelz and save it to Firebase Storage.
+		saveImage(req.body.processedImageURL, req.body.imageTicket);
+
+		return res.status(200).end();
+	}
+
+	res.status(400).end();
 });
 
 function saveImage(url, ticketId) {
+
 	// Generate a random HEX string using crypto (a native node module).
 	const randomFileName = crypto.randomBytes(16).toString('hex');
+
 	// Fetch image info using a HTTP HEAD request.
 	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/HEAD
 	request.head(url, (error, info) => {
-	if (error) {
-		return console.error(error);
-	}
-	// Download image from Pixelz, then save the image to Firebase
-	// using the Google Cloud API and the magic of Node Streams.
-	// https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/v0.52.0/storage/file
-	// http://stackoverflow.com/questions/28355079/how-do-node-js-streams-work
-	request(url)
-		.pipe(
-			bucket.file(`sample/images/${randomFileName}`)
-			.createWriteStream({
-  				metadata: {
-    				contentType: info.headers['content-type']
-  				}
-			})
-		)
-		.on('error', (err) => {
-			// Do something if the upload fails.
-			console.error(err);
-		})
-		.on('finish', () => {
-			// Do something when everything is done.
+  	if (error) {
+  		return console.error(error);
+  	}
 
-			// Get download url for stored image
-			console.log('Image successfully uploaded to Firebase Storage!');
-		});
+  	// Download image from Pixelz, then save the image to Firebase
+  	// using the Google Cloud API and the magic of Node Streams.
+  	// https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/v0.52.0/storage/file
+  	// http://stackoverflow.com/questions/28355079/how-do-node-js-streams-work
+  	request(url)
+  		.pipe(bucket.file(`sample/images/${randomFileName}`)
+        .createWriteStream({
+          metadata: {
+            contentType: info.headers['content-type']
+          }
+        })
+      ).on('error', (error) => {
+
+        // Do something if the upload fails.
+  			console.error(error);
+  		}).on('finish', () => {
+  			console.log('Image successfully uploaded to Firebase Storage!');
+
+        // Configure image access
+        const config = {
+          action: 'read',
+          expires: '05-03-2027'
+        };
+
+        // Get download url for stored image
+        bucket.file(`sample/images/${randomFileName}`)
+          .getSignedUrl(config, (error, url) => {
+            if (error) {
+              return console.error(error);
+            }
+
+            // Do something with your URL
+            // (Like save it to Firebase)
+
+            console.log(`Your image Firebase Storage URL is: ${url}`);
+          });
+  		});
 	});
-	getImageOwner(/* get ticket and downloadURL to use in function */);
 };
-
-function getImageOwner (ticketId, downloadURL) {
-	// Loop over all tickets in database and find the match
-	// Get the image owner
-	// Store downloadURL for Firebase to owner/processedImages
-}
